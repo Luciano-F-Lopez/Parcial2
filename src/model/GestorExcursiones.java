4713b2e37b9021241ed4d793454d0e0fafeaf4b4
Luciano-F-Lopez <lucianolopez717@gmail.com>
2024-11-29 10:56:57 -0300

    mas mitad de parcial

--- src/model/GestorExcursiones.java
+++ src/model/GestorExcursiones.java
@@ -6,6 +6,9 @@ package model;
 
 import java.io.File;
 import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+import java.util.function.Predicate;
 
 /**
  *
@@ -17,9 +20,34 @@ public class GestorExcursiones {
     public GestorExcursiones(String filePath){
         File file =new File(filePath);
         if (file.exists()){
-            repository = Repository.loadFromFile(filePath)
+            repository = Repository.loadFromFile(filePath);
         } else {
             repository = new Repository<>(filePath,new ArrayList<>());
         }
     }
+    
+    public boolean agregarExcursion(Excursion excursion){
+        if(excursion == null){
+            throw new IllegalArgumentException("la excursion no puede ser nula"); 
+        }
+        repository.add(excursion);
+        repository.save();
+        return true;
+    }
+    
+    public Optional<Excursion> buscarPorId(int id){
+        return repository.getAll().stream().filter(excursion -> excursion.getId() == id).findFirst();
+    }
+    
+    public List<Excursion> filtrarExcursion(Predicate<Excursion> criterio){
+        return repository.getAll().stream().filter(criterio).toList();
+    }
+    
+    public List <Excursion> consultarExcursionesPorPrecio(double precioMin){
+        if (precioMin < 0){
+            throw new IllegalArgumentException("el precio minimo no puede ser menor a 0"); 
+        }
+        return repository.getAll().stream().filter(excursion -> excursion.calcularPrecioFinal() >= precioMin).toList();
+    }
+    
 }
--- src/model/GestorReservas.java
+++ src/model/GestorReservas.java
@@ -4,19 +4,64 @@
  */
 package model;
 
+import java.io.File;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
+import java.util.function.Predicate;
 
 /**
  *
  * @author MIPC
  */
 public class GestorReservas {
-
-    boolean realizarReserva(String cliente, int cupoMaximo) {
-        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    
+    private Repository<Reserva> repository;
+    private final int cupoMaximo;
+    
+    public GestorReservas(String filePath,int cupoMaximo){
+        if(cupoMaximo <= 0){
+            throw new IllegalArgumentException("el cupo maximo no puede ser menor ni igual a 0"); 
+        } 
+        this.cupoMaximo = cupoMaximo;
+        
+        File file = new File(filePath);
+        if (file.exists()){
+            this.repository = Repository.loadFromFile(filePath);
+        } else {
+            this.repository = new Repository<>(filePath,new ArrayList<>());
+        }
     }
+    
 
+    public boolean realizarReserva(Reserva reserva ) {
+        if(reserva == null){
+            throw new IllegalArgumentException("la reserva no puede ser nula"); 
+        } 
+        
+       if(repository.getAll().size() >= cupoMaximo){
+           System.out.println("No hay cupos disponibles");
+           return false;       
+       }
+       repository.add(reserva);
+       repository.save();
+       return true;
+    }
+   
+    public List<Reserva> getReservas(){
+        return repository.getAll();
+    }
+    
+    
+    public double calcularIngresos(Predicate<Reserva> filtro){
+        return repository.getAll().stream().filter(filtro).mapToDouble(Reserva::getPrecio).sum();
+    }
+    
+    public double  calcularIngresosTotales(){
+        return calcularIngresos(Reserva::isPagoConfirmado);
+    }
+    
+    
     Optional<List<Reserva>> traerReservas(String generarFilepath) {
         throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
     }
